ac6de286162eaaf61b476bd77e923370dc16d416
freecates <ramongil@ramongil.cat>
2015-02-17 07:28:40 +0100

    Cavni de color de bg, logo a posts i nova versió del worker.

--- worker.js
+++ worker.js
@@ -5,8 +5,8 @@ importScripts('/bower_components/cache-polyfill/dist/serviceworker-cache-polyfil
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
 
-var DEFAULT_CACHE = 'figurairetrat-app-cache4';
-var AIP_CACHE = 'figurairetrat-aip-cache4';
+var DEFAULT_CACHE = 'figurairetrat-app-cache3';
+var AIP_CACHE = 'figurairetrat-aip-cache3';
 
 console.log('SW startup', DEFAULT_CACHE);
 
